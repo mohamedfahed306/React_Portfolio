47dd491f419a99ae8cedf59c510f9ebf4349e752
mohamedfahed306 <a.mohamedfahed001@gmail.com>
2026-04-15 19:59:54 +0530

    Add new file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,44 @@
-import "./about.css";
+import "./About.css";
 
 const About = () => {
   return (
-    <div id='about' className='about'>About</div>
+    <div id='about' className='about'>
+      <div className="about-title">
+        <h1>About Me</h1>
+        <p>What i do</p>
+      </div>
+      
+      <div className="about-container">
+        <div className="about-img">
+          <div className="about-img-cover">
+           <img src="fahed-dp.jpg" alt="" />
+          </div>
+        </div>
+        <div className="about-details">
+          <h4>I'm a Web Designer & </h4>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur eaque deleniti sit exercitationem minus eos quae, possimus corporis aut sint animi odio, fugiat voluptas expedita consectetur cumque ipsum molestias explicabo accusamus natus reprehenderit omnis modi. Enim itaque omnis voluptatibus facere eveniet reprehenderit neque est nostrum possimus? Tenetur cum quia quibusdam?</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur eaque deleniti sit exercitationem minus eos quae, possimus corporis aut sint animi odio, fugiat voluptas expedita consectetur cumque ipsum molestias explicabo accusamus natus reprehenderit omnis modi. Enim itaque omnis voluptatibus facere eveniet reprehenderit neque est nostrum possimus? Tenetur cum quia quibusdam?</p>
+          <div className="about-boxes">
+            <div className="about-box">
+              <h2>07</h2>
+              <p>Years of Experience</p>
+            </div>
+            <div className="about-box">
+              <h2>07</h2>
+              <p>Years of Experience</p>
+            </div>
+            <div className="about-box">
+              <h2>07</h2>
+              <p>Years of Experience</p>
+            </div>
+          </div>
+          <div className="about-buttons">
+            <button>Hire Me</button>
+            <button>Download CV</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,65 @@
+const cardDetails = [
+    {
+        image :"fahed-dp.jpg",
+        title:"Portfolio",
+        skills:["HTML","CSS","JavaScript","React"],
+    },
+    {
+        image :"fahed-dp.jpg",
+        title:"Portfolio",
+        skills:["HTML","CSS","JavaScript","React"],
+    },
+    {
+        image :"fahed-dp.jpg",
+        title:"Portfolio",
+        skills:["HTML","CSS","JavaScript","React"],
+    },
+    {
+        image :"fahed-dp.jpg",
+        title:"Portfolio",
+        skills:["HTML","CSS","JavaScript","React"],
+    },
+    {
+        image :"fahed-dp.jpg",
+        title:"Portfolio",
+        skills:["HTML","CSS","JavaScript","React"],
+    },
+]
+function UserCard(props){
+    return <div className="project-container">
+            <div className="project-card">
+                <div className="project-img">
+                    <img src={props.image} />
+                </div>
+                <div className="project-title">
+                    <h3>{props.title}</h3></div>
+                <div className="project-blog"><a href="#">Read more</a></div>
+                <div className="project-tools">
+                    <ul>
+                        {props.skills.map((skill,index)=>(
+                            <li key={index}>{skill}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+}
+
+const ProjectCard = (props) => {
+  return (
+    <>
+    {
+        cardDetails.map((user,index) => (
+            <UserCard key={index}
+            image={user.image}
+            title={user.title}
+            skills={user.skills} />
+    
+        ))
+
+    }
+    </>
+  )
+}
+
+export default ProjectCard
